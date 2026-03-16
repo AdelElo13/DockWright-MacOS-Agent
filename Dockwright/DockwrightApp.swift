2731e8925fe547453724d4061385ecc20921540d
AdelElo13 <123841129+AdelElo13@users.noreply.github.com>
2026-03-16 02:37:30 +0100

    Initial Commit

---
+++ Dockwright/DockwrightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockwrightApp.swift
+//  Dockwright
+//
+//  Created by Adel  on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DockwrightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
